0c42f31bfbce79f703c8e1b7c81dd28f339fb8b3
Daniel Ramos <daniel.rsi6oa@gmail.com>
2026-09-18 22:03:00 -0300

    agrego metodos varios y prueba de esos metodos

--- src/main/service/CompararCartas.java
+++ src/main/service/CompararCartas.java
@@ -1,5 +1,29 @@
 package main.service;
 
 public class CompararCartas {
-    
+    public int mayorValor(int[] cartas) {
+        int mayor = cartas[0];
+
+
+        //En su momento, las cartas la recorro con un toArray
+        for (int i = 1; i < cartas.length; i++) {
+            if (cartas[i] > mayor) {
+                mayor = cartas[i];
+            }
+        }
+
+        return mayor;
+    }
+
+    public int cantidadMayor(int[] cartas, int mayor) {
+        int cantidad = 0;
+
+        for (int i = 0; i < cartas.length; i++) {
+            if (cartas[i] == mayor) {
+                cantidad++;
+            }
+        }
+
+        return cantidad;
+    }
 }
--- src/main/service/PruebaComparar.java
+++ src/main/service/PruebaComparar.java
@@ -1,5 +1,19 @@
 package main.service;
 
 public class PruebaComparar {
-    
+    public static void main(String[] args) {
+        
+        CompararCartas comparador = new CompararCartas();
+
+        // Ejemplo de cartas de los jugadores 1, 2, 3, 4 después de mezclar y repartir
+        // Prueba en donde no se repiten valores, osea, no hay empate
+        int[] cartasRonda1 = {8,12,5,10};
+
+        int mayor = comparador.mayorValor(cartasRonda1);
+        int cantidadMayor = comparador.cantidadMayor(cartasRonda1, mayor);
+
+        System.out.println("Ronda 1:");
+        System.out.println("Carta de mayor valor numerico: " + mayor);
+        System.out.println("Cantidad de cartas con ese valor: " + cantidadMayor);
+    }
 }
