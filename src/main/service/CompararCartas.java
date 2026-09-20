f18c8076ed8970b0230766f41147d2e3876a1571
Daniel Ramos <daniel.rsi6oa@gmail.com>
2026-09-19 21:06:46 -0300

    Agregue funcion ganadorRonda

--- src/main/service/CompararCartas.java
+++ src/main/service/CompararCartas.java
@@ -2,10 +2,9 @@ package main.service;
 
 public class CompararCartas {
     public int mayorValor(int[] cartas) {
+
         int mayor = cartas[0];
 
-
-        //En su momento, las cartas la recorro con un toArray
         for (int i = 1; i < cartas.length; i++) {
             if (cartas[i] > mayor) {
                 mayor = cartas[i];
@@ -25,5 +24,25 @@ public class CompararCartas {
         }
 
         return cantidad;
+    }
+
+    public int ganadorRonda(int[] cartas) {
+
+        int mayor = mayorValor(cartas);
+
+        int cantidadMayor = cantidadMayor(cartas, mayor);
+
+        if (cantidadMayor > 1) {
+            return -1;
+        }
+
+        for (int i = 0; i < cartas.length; i++) {
+
+            if (cartas[i] == mayor) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
